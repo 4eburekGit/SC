548cc2acb971ec83484ddf80d4df36a030de4e51
4eburekGit <165718030+4eburekGit@users.noreply.github.com>
2026-03-25 22:55:08 +0300

    hotfix server-launch.sh
    forgot to add database population

--- server/server-launch.sh
+++ server/server-launch.sh
@@ -8,6 +8,20 @@ echo "Starting database container"
 
 sudo docker compose up -d
 
+echo "Populating database"
+
+sudo docker exec -i postgres psql db -U kottsov -c "CREATE DATABASE scserverdata"
+
+sudo docker exec -i postgres psql db -U kottsov -c "CREATE DATABASE scserverdata_test"
+
+sudo docker cp scserverdata.sql postgres:/tmp/scserverdata.sql
+
+sudo docker cp scserverdata_test.sql postgres:/tmp/scserverdata_test.sql
+
+sudo docker exec -i postgres psql -U kottsov -d scserverdata -f /tmp/scserverdata.sql
+
+sudo docker exec -i postgres psql -U kottsov -d scserverdata_test -f /tmp/scserverdata_test.sql
+
 echo "Building..."
 
 sudo docker build -t server .
